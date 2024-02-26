4eb2d110c6cdc1a3b218c0277aa36934703842e9
Ivan Sevastynaov <ivan.sevastyanov@waveaccess.global>
2024-02-26 14:05:12 +0300

    main_meni template created

--- BankAccount.py
+++ BankAccount.py
@@ -1,13 +1,22 @@
+from datetime import datetime
+
+class Transaction:
+    def __init__(self, account, date, type, amount):
+        self.account = account
+        self.date = date
+        self.type = type
+        self.amount = amount
+
 class BankAccount:
     def __init__(self, owner):
         self.owner = owner
         self.balance = 0
+        self.history = []
     
     def deposit(self, amount):
         self.balance += amount
+        self.history.append(Transaction(self, datetime.now(), "deposit", amount))
     
     def withdraw(self, amount):
         self.balance -= amount
-    
-    def get_balance(self):
-        return self.balance
+        self.history.append(Transaction(self, datetime.now(), "withdrawal", amount))
--- User.py
+++ User.py
@@ -1,13 +1,13 @@
 class User:
     def __init__(self, username):
         self.username = username
-        self._password = ''
-    
-    def set_password(self, password):
-        self._password = password
+        self.bank_account = ''
     
     def get_username(self):
         return self.username
     
-    def _retrieve_password(self):
-        return self._password
+    def set_account(self, account):
+        self.bank_account = account
+
+    def get_history(self):
+        return self.bank_account.history
